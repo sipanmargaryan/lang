fe90d2593c30d56e5c29c36fb4cef700853bcd08
Sipan Margaryan <sipan.margaryan@sentium-consulting.com>
2023-05-31 11:28:00 +0400

    add

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ from agents import search_agent
 search = st.text_input("Search for product")
 
 if search:
-    # list = get_products_list(search)
-    # st.table(list)
     agent_chain = search_agent()
     answer = agent_chain.run(input=search)
-    # st.write(answer["intermediate_steps"][3])
     products = ast.literal_eval(answer["intermediate_steps"][3])
-    print(products)
     st.table([dict(name=item[0], price=item[1], id=item[2], description=item[3]) for item in products])
